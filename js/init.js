cae42af9062fb61204b9d1eb1b68376648de7fe4
debrah <binyanyadebby@gmail.com>
2021-03-23 19:37:00 -0700

    changes on the card content

--- js/init.js
+++ js/init.js
@@ -2,6 +2,7 @@
   $(function(){
     $('.sidenav').sidenav();
     $('.slider').slider();
+    intervals:5000
     $('.modal').modal();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
