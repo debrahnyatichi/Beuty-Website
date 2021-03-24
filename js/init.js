372c6a0c00e78e8c5320613e252925924fbb5f1b
debrah <binyanyadebby@gmail.com>
2021-03-23 17:42:22 -0700

    changes on card titles

--- js/init.js
+++ js/init.js
@@ -1,8 +1,7 @@
 (function($){
   $(function(){
-
     $('.sidenav').sidenav();
     $('.slider').slider();
-
+    $('.modal').modal();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
